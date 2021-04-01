cef136bb1d3f8582c56f25aad6871030b2b58f6f
FreeNikita <freenikita95@gmail.com>
2021-04-02 00:37:48 +0300

    add basic API

--- src/App.js
+++ src/App.js
@@ -7,23 +7,26 @@ import Sidebar from 'components/Sidebar';
 import Header from 'components/Header';
 import { routing, HOME_PAGE } from './configs/routing';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
+const useStyles = makeStyles((theme) => {
+  console.log('theme', theme);
+  return ({
+    root: {
+      display: 'flex',
+    },
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  });
+});
 
 function App() {
   const classes = useStyles();

27c0ae792f98f108bfd0c4d6443e4f792c07b8e6
FreeNikita <freenikita95@gmail.com>
2021-06-18 00:34:02 +0300

    add new page

--- src/configs/routing.js
+++ src/configs/routing.js
@@ -1,17 +1,24 @@
 import { lazy } from 'react';
 
+const DashboardPage = lazy(() => import('pages/Dashboard'));
 const HomePage = lazy(() => import('pages/Home'));
 const PetPage = lazy(() => import('pages/Pet'));
 
-export const HOME_PAGE = '/dashboard';
+export const HOME_PAGE = '/';
+export const PETS_PAGE = '/pets';
 export const PET_PAGE = '/pet';
 export const OPEN_PET_PAGE = `${PET_PAGE}/:id`;
 export const ADD_PET_PAGE = `${PET_PAGE}/create`;
 
 export const routing = [
   {
-    component: HomePage,
+    component: DashboardPage,
     path: HOME_PAGE,
+    exact: true,
+  },
+  {
+    component: HomePage,
+    path: PETS_PAGE,
     exact: true,
   },
   {
